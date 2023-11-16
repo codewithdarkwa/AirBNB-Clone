973d64e4f570b6b90dd5f41068b74cf073ba7aa8
codewithdarkwa <jnrdrkw1@gmail.com>
2023-11-16 19:44:34 +0000

    Initial Commit

---
+++ AirBNB/AirBNBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBNBApp.swift
+//  AirBNB
+//
+//  Created by Darkwa John on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirBNBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBNB/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AirBNB
+//
+//  Created by Darkwa John on 16/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
